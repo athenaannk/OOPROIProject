8ee79a7eec2766ece6c00db76accf62eab67a85b
Athena Kostro <home@Homes-MacBook-Pro.local>
2023-03-13 14:58:15 -0400

    loopy

--- biggerpockets.py
+++ biggerpockets.py
@@ -10,10 +10,38 @@ class RetInvestment():
 #combine all monthly income
     def income(self):
         print(f"\nLet's figure out your total monthly expenses.")
-        self.rent = int(input("\nHow much rent are you charging your tenant? Enter numbers only no comma, decimal, or special character."))
-        self.laundry = input("How much are you charging your tenant for laundry? Enter numbers only no comma, decimal, or special character. If none, enter '0'. ")
-        self.storage = input("How much are you charging your tenant for storage? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
-        self.misc = input("Please enter the total of any misc. charges to your tenant that have not been previously asked? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
+        while True:
+            self.rent = input(f"\nHow much rent are you charging your tenant? Enter numbers only no comma, decimal, or special character. For none type '0'.")
+            try:
+                self.rent = int(self.rent)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.laundry = input(f"\nHow much are you charging your tenant for laundry services? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.laundry = int(self.laundry)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.storage = input(f"\nHow much are you charging your tenant for storage? Enter numbers only no comma, decimal, or special character. For none type '0'.")
+            try:
+                self.storage = int(self.storage)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.misc = input(f"\nPlease enter the total of any misc. charges to your tenant that have not been previously asked? Enter numbers only no comma, decimal, or special character. For none type '0'.")
+            try:
+                self.misc = int(self.misc)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+     
         self.total_monthly_income = int(self.rent) + int(self.laundry) + int(self.storage) + int(self.misc)
         print(f"\nYour total monthly income on the property is ${self.total_monthly_income}.")
 
@@ -21,22 +49,93 @@ class RetInvestment():
 #combine all expenses
     def expenses(self):
         print(f"\nLet's figure out your total expenses.")
-        self.tax = input(f"\nWhat is the total amount of taxes you pay on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. ")
-        self.insurance = input(f"What is the total amount you pay to insure the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. ")      
-        self.utilities =input(f"What is the total amount of utilities you pay on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character.")
-        self.HOA =input(f"What is the total amount pay towards the HOA on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
-        self.lawn_snow_care =input(f"What is the total amoun you pay on lawn care and snow removal rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
-        self.vacancy =input(f"What is the total amount you pay towards vacancy rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
-        self.repairs =input(f"What is the total amount you pay towards repairs on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
-        self.CapEx=input(f"What is the total amount of you pay towards capital expenditures on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
-        self.property_management=input(f"What is the total amount you pay on property management on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character.")
-        self.mortgage=input(f"What is the total mortgage for the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character.")
+        while True:
+            self.tax = input(f"\nWhat is the total amount of taxes you pay on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.tax = int(self.tax)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.insurance = input(f"\nWhat is the total amount you pay to insure the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.insurance = int(self.insurance)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.utilities = input(f"\nWhat is the total amount of utilities you pay on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.utilities = int(self.utilities)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.HOA = input(f"\nWhat is the total amount pay towards the HOA on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.HOA = int(self.HOA)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.lawn_snow_care = input(f"\nWhat is the total amount you pay on lawn care and snow removal rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.lawn_snow_care = int(self.lawn_snow_care)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.vacancy = input(f"\nWhat is the total amount you pay towards vacancy rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.vacancy = int(self.vacancy)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+        
+        while True:
+            self.repairs = input(f"\nWhat is the total amount you pay towards repairs on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.repairs = int(self.repairs)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.CapEx = input(f"\nWhat is the total amount you pay towards capital expenditures on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.CapEx = int(self.CapEx)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.property_management = input(f"\nWhat is the total amount you pay on property managment on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.property_management = int(self.property_management)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.mortgage = input(f"\nWhat is the total mortgage on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.mortgage = int(self.mortgage)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
         self.total_monthly_expenses = self.tax + int(self.insurance) + int(self.utilities) + int(self.HOA) + int(self.lawn_snow_care) + int(self.vacancy) + int(self.repairs) + int(self.CapEx) + int(self.property_management) + int(self.mortgage)
+
         print(f"\nYour total monthly expenses on the property are ${self.total_monthly_expenses}")
 
 #annual cash flow is income *12 - expenses
     def cashFlow(self):
-        self.total_monthly_cash_flow = self.total_monthly_expenses - self.total_monthly_income
+        self.total_monthly_cash_flow = self.total_monthly_income - self.total_monthly_expenses
         print(f"\nYour total monthly cash flow is ${self.total_monthly_cash_flow}.")
         self.total_annual_cash_flow = self.total_monthly_cash_flow * 12
         print(f"\nYour total annual cash flow is ${self.total_annual_cash_flow}. ")
@@ -44,11 +143,40 @@ class RetInvestment():
 #calculate investment
     def investment(self):
         print(f"\nLet's figure out your investment total.")
-        self.down_payment = input(f"\nHow much did you put down on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character.")
-        self.closing_costs = input(f"How much were your closing costs on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character.")
-        self.rehab_budget = input(f"How much did you budget for rehab on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character.")
-        self.down_payment = input(f"Are there any other expenses torward the purchase of the property you would like to enter rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. If none, enter '0'.")
-        self.total_investment = int(self.down_payment) + int(self.closing_costs) + int(self.rehab_budget) + int(self.down_payment)
+
+        while True:
+            self.down_payment = input(f"\nHow much did you put down on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.down_payment = int(self.down_payment)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.closing_costs = input(f"\nHow much were your closing costs on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.closing_costs = int(self.closing_costs)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.rehab_budget = input(f"\nHow much did you budget for rehab on the property rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.rehab_budget = int(self.rehab_budget)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+
+        while True:
+            self.other = input(f"\nAre there any other expenses torward the purchase of the property you would like to enter rounded to the nearest dollar? Enter numbers only no comma, decimal, or special character. For none type '0'")
+            try:
+                self.other = int(self.other)
+                break
+            except:
+                print(f"\nHey fast fingers...you have to enter a number with no commas, decimals, or special characters. Try again.")
+        
+        self.total_investment = int(self.down_payment) + int(self.closing_costs) + int(self.rehab_budget) + int(self.other)
         print(f"\nYour total investment on the property is ${self.total_investment}")
     
     def ROI(self):
